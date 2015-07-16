617211d4f2744dbfeab330264b9ead730f565935
Abhishek Choubey <abhishek.choubey@consultadd.com>
2015-07-16 07:44:52 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rortraining::Application.config.secret_token = '0890285e1dee9c2af30c9000198b89292baf7279513fc58c41f34e27a044daa2d781f6f977bbdcb5e4ac12aa0542ec77a21d9fc925b0804d19fcb9dfa8be0a0a'
