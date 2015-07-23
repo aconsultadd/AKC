87c4e723f0c8db12b195b833a2f6b181b4c23b78
Abhishek Choubey <abhishek.choubey@consultadd.com>
2015-07-23 09:06:40 +0530

    Created Pages and customized bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rortraining::Application.routes.draw do
- 
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +52,7 @@ Rortraining::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
+ 
 
   # See how all your routes lay out with "rake routes"
 
